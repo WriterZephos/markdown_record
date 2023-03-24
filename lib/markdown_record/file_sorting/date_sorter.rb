0f7f54a073ff1f2b9aea2a7f3dd53bcd3881cefc
Bryant Morrill <bryantreadmorrill@gmail.com>
2023-03-23 18:46:55 -0600

    hot fix for date sorter

--- lib/markdown_record/file_sorting/date_sorter.rb
+++ lib/markdown_record/file_sorting/date_sorter.rb
@@ -3,7 +3,7 @@ module MarkdownRecord
     class DateSorter < MarkdownRecord::FileSorting::Base
       def initialize(sort_regex = /^(\d\d\d\d?_\d\d?_\d\d?)/, date_pattern = "%Y_%m_%d")
         super(sort_regex)
-        @date_pattern = options[:date_pattern] || "%Y_%m_%d"
+        @date_pattern = date_pattern || "%Y_%m_%d"
       end
 
       def to_sort_value(value)
